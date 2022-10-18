83984c0a6feedaebc96caa1d6afd2d19faee9242
RomanZhirnov <rvzh@mail.ru>
2022-10-18 15:56:49 +0300

    Fix all mentor comments

--- Assets/Scripts/AlarmSignalization.cs
+++ Assets/Scripts/AlarmSignalization.cs
@@ -9,8 +9,9 @@ public class AlarmSignalization : MonoBehaviour
     [SerializeField] private AudioClip _alarmSignal;
 
     private AudioSource _audioSource;
+    private Coroutine _playAlarmSignal;
     private float _changingSpeed = 0.1f;
-    private float _targetVolume = 1f;
+    private float _targetVolume;
     private float _currentVolume = 0f;
 
     private void Start()
@@ -23,7 +24,13 @@ public class AlarmSignalization : MonoBehaviour
         if (collision.TryGetComponent<PlayerController>(out PlayerController playerController))
         {
             _targetVolume = 1f;
-            StartCoroutine(AlarmPlay());
+
+            if (_playAlarmSignal != null)
+            {
+                StopCoroutine(_playAlarmSignal);
+            }
+
+            _playAlarmSignal =  StartCoroutine(AlarmPlay(_targetVolume));
         }
     }
 
@@ -32,16 +39,23 @@ public class AlarmSignalization : MonoBehaviour
         if (collision.TryGetComponent<PlayerController>(out PlayerController playerController))
         {
             _targetVolume = 0f;
+
+            if (_playAlarmSignal != null)
+            {
+                StopCoroutine(_playAlarmSignal);
+            }
+
+            _playAlarmSignal = StartCoroutine(AlarmPlay(_targetVolume));
         }
     }
 
-    private IEnumerator AlarmPlay()
+    private IEnumerator AlarmPlay(float targetVolume)
     {
         Debug.Log("Alarm ON");
 
         do
         {
-            _currentVolume = Mathf.MoveTowards(_currentVolume, _targetVolume, _changingSpeed);
+            _currentVolume = Mathf.MoveTowards(_currentVolume, targetVolume, _changingSpeed);
 
             if (_currentVolume <= 0)
             {
