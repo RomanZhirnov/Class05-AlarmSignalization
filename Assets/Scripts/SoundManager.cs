45db22ee064f4997a2cff18b7aea5aafda956d4f
RomanZhirnov <rvzh@mail.ru>
2022-10-19 14:12:30 +0300

    FixAll

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -4,14 +4,12 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    [SerializeField] private AudioClip _alarmSignal;
     [SerializeField] private Alarm _alarm;
 
     private AudioSource _audioSource;
     private Coroutine _playAlarmSignal;
     private float _changingSpeed = 0.1f;
     private float _targetVolume;
-    //private float _currentVolume = 0f;
 
     private void Start()
     {
@@ -31,30 +29,6 @@ public class SoundManager : MonoBehaviour
         _alarm.Entered -= Play;
         _alarm.Leaving -= Stop;
     }
-
-    //private void SoundPlay()
-    //{
-
-    //    if (_playAlarmSignal != null)
-    //    {
-    //        StopCoroutine(_playAlarmSignal);
-    //    }
-
-    //    _targetVolume = 1f;
-    //    _playAlarmSignal = StartCoroutine(AlarmPlay(_targetVolume));
-
-
-    //    else if (shouldTurnOn = false & _currentVolume != 0)
-    //    {
-    //        if (_playAlarmSignal != null)
-    //        {
-    //            StopCoroutine(_playAlarmSignal);
-    //        }
-
-    //        _targetVolume = 0f;
-    //        _playAlarmSignal = StartCoroutine(AlarmPlay(_targetVolume));
-    //    }
-    //}
 
     private void Play()
     {
@@ -80,23 +54,11 @@ public class SoundManager : MonoBehaviour
 
     private IEnumerator AlarmPlay(float targetVolume)
     {
-        //do
-        //{
-        //    _currentVolume = Mathf.MoveTowards(_currentVolume, targetVolume, _changingSpeed);
-        //    //Debug.Log($"current volume - {_currentVolume} target volume - {_targetVolume}");
-        //    _audioSource.PlayOneShot(_alarmSignal, _currentVolume);
-        //    yield return new WaitWhile(() => _audioSource.isPlaying);
-
-        //} while (_currentVolume > 0 & _currentVolume < 1);
-        
-
-
         while (_audioSource.volume != targetVolume)
         {
             _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, targetVolume, _changingSpeed);
+            Debug.Log(_audioSource.volume);
             yield return new WaitForSeconds(1f);
         }
-
-        //Debug.Log("Alarm OFF");
     }
 }
